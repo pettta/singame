6e7d76e788218bd0e24b2455ea7d6d1ec7b01689
Melogale <zachariabadgers@gmail.com>
2021-05-28 11:41:48 -0400

    goat

--- src/sin/mundus/materia/entity/EntityGoat.java
+++ src/sin/mundus/materia/entity/EntityGoat.java
@@ -15,29 +15,12 @@ import java.util.Random;
 
 public class EntityGoat extends Entity {
 
-    private int counter;
-    private Boolean slamming;
-    Direction slamDirection;
-    private int spriteIndex;
-    private int slamDelay;
+    int spriteIndex;
+    int cycler;
 
     Polysprite idle;
     Polysprite walk;
     Polysprite attack;
-
-    public BufferedImage rock;
-
-    public int[] posx;
-    public int[] posy;
-    int pos = 0;
-    boolean moving;
-    int cycler;
-    int moveTimer;
-    int moveDelay;
-    int gunTicker;
-
-    BufferedImage bar;
-    BufferedImage bartop;
 
     int state;
 
@@ -57,30 +40,16 @@ public class EntityGoat extends Entity {
 
         image = idle.getCurImage(0);
         hb = new Rectangle((int) x, (int) y, width, height);
-        slamming = false;
 
     }
     public JSONObject write(JSONObject obj) {
         JSONObject extra = super.write(obj);
-        extra.put("counter", counter);
         extra.put("spriteIndex", spriteIndex);
         return extra;
     }
 
-    public void doMove() {
-        if(!slamming) {
-            Random random = new Random();
-            int r = random.nextInt(2);
-            pos = pos == 1 ? r == 0 ? 0 : 2 : pos == 2 ? r == 0 ? 0 : 1 : pos == 0 ? r == 0 ? 1 : 2 : 0;
-            this.x = posx[pos];
-            this.y = posy[pos];
-            moveDelay = 40;
-        }
-    }
-
     public ISaveable read(JSONObject obj) {
         super.read(obj);
-        counter = obj.getInt("counter");
         spriteIndex = obj.getInt("spriteIndex");
         return this;
     }
@@ -90,7 +59,6 @@ public class EntityGoat extends Entity {
             if (ent.getType() == EntityType.Rock) {
                 if (getBounds().intersects(ent.getBounds())) {
                     handler.delEnt(ent);
-                    // Add line for damage animation here i think?
                     health -= 34;
                 }
             }
@@ -99,100 +67,18 @@ public class EntityGoat extends Entity {
 
     public void tick() {
         Vector toPlayer = new Vector(getXMid(), getYMid(), game.player.getXMid(), game.player.getYMid());
-        Vector playerLoc = new Vector(getXMid(), getYMid(), game.player.getXMid(), game.player.getYMid(), 30);
         float distance = toPlayer.getMagnitude();
-        if(distance < 400) {
-            game.hud.drawb1 = true;
-            game.hud.b1health = (int) health;
-        } else {
-            game.hud.drawb1 = false;
-        }
-        slamDelay--;
-        moveDelay--;
-        if ((distance < 60 || slamming) && slamDelay < 0 && !moving) {
-            counter++;
-            if(!slamming) {
-                velX = playerLoc.getHorizComp();
-                velY = playerLoc.getVertComp();
-                slamDirection = Vector.getCardinalDirectionMirrored(velX, velY);
-                image = attack.getCurImage(0, slamDirection, slamDirection);
-                slamming = true;
-            }
-        }
-        if(slamming && counter < 20) {
-            image = attack.getCurImage(counter < 7 ? 0 : 1, slamDirection, slamDirection);
-        }
-        if (counter >= 20 && counter < 30){
-            image = attack.getCurImage(counter < 21 ? 2 : 3, slamDirection, slamDirection);
-            if(counter > 26 && counter < 35) {
-                Rectangle slambox = null;
-                if(slamDirection == Direction.W) {
-                    slambox = new Rectangle((int) x + 16, (int) y + 32, 80, 24);
-                }
-                if(slamDirection == Direction.E) {
-                    slambox = new Rectangle((int) x + 16 - 80, (int) y + 32, 80, 24);
-                }
-                if(slamDirection == Direction.N) {
-                    slambox = new Rectangle((int) x, (int) y - 32, 32, 80);
-                }
-                if(slamDirection == Direction.S) {
-                    slambox = new Rectangle((int) x, (int) y + 64, 32, 80);
-                }
-                if(slambox.intersects(game.player.getBounds())) {
-                    game.player.health -= 10;
-                }
-            }
-        }
-        if(counter >= 30 && counter < 35) {
-            image = attack.getCurImage(counter <= 31 ? 4 : counter <= 32 ? 5 : counter <= 33 ? 6 : 7, slamDirection, slamDirection);
-        }
-        if(counter > 40) {
-            slamming = false;
-            counter = 0;
-            slamDelay = 40;
+
+        cycler = Lib.cycle(cycler, 0, 2);
+        if(cycler == 2) {
+            spriteIndex = Lib.cycle(spriteIndex, 0, 1);
+            image = idle.getCurImage(spriteIndex, toPlayer.getDirection());
         }
 
-        if(distance > 60 && distance < 500) {
-            if(gunTicker > 7) {
-                EntityWormBullet proj = new EntityWormBullet(getXMid(), getYMid() - 20, game, rock);
-                Vector vector = new Vector(getXMid(), getYMid() - 20, game.player.getXMid(), game.player.getYMid() - 20, 7);
-                proj.setVelX(vector.getHorizComp());
-                proj.setVelY(vector.getVertComp());
-                handler.addEnt(proj);
-                gunTicker = 0;
-            }
-            gunTicker++;
-        }
-        cycler = Lib.cycle(cycler, 0, 3);
-        if(cycler == 2) {
-            spriteIndex = Lib.cycle(spriteIndex, 0, 7);
-        }
-        if(!slamming) {
-            //image = ps.getCurImage(spriteIndex, getDirectionToPlayer(), Direction.S);
-        }
         doDamage();
         if (health <= 0) {
-            // This is where a death animation would go I think
             handler.delEnt(this);
-            game.hud.drawb1 = false;
-        }
-        Random random = new Random();
-        if(random.nextInt(1000) > 980 && !moving && !slamming && moveDelay < 0) {
-            moving = true;
-            moveTimer = 0;
-        }
-        if(moving) {
 
-            if(moveTimer < 7) {
-                //image = sps.getCurImage(moveTimer, getDirectionToPlayer(), Direction.S);
-            } else if(moveTimer == 7) {
-                doMove();
-            } else if(moveTimer > 7 && moveTimer < 15) {
-                //image = eps.getCurImage(moveTimer - 8, getDirectionToPlayer(), Direction.S);
-            } else {
-                moving = false;
-            }
-            moveTimer++;
         }
 
     }
@@ -204,19 +90,11 @@ public class EntityGoat extends Entity {
 
 
     public void render(Graphics g) {
-        if(image.getWidth() == 32) {
-            g.drawImage(image.getSubimage(0, 32, 32, 32), (int) x, (int) y + 32, null);
-        } else {
-
-        }
+        g.drawImage(image, (int) x, (int) y, null);
     }
 
     public void renderTop(Graphics g) {
-        if(image.getWidth() == 32) {
-            g.drawImage(image.getSubimage(0, 0, 32, 32), (int) x, (int) y, null);
-        } else {
-            g.drawImage(image, (int) x - 80, (int) y - 32, null);
-        }
+
     }
 
 }
